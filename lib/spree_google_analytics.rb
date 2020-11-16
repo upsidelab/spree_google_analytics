e74dfb1ce2a4237e0d6c2481aa185f43623facb1
Grzegorz Tłuszcz <grzegorz.tluszcz@upsidelab.io>
2020-11-16 12:08:12 +0100

    webpacker + engine - step 3 - configure webpacker instance

--- lib/spree_google_analytics.rb
+++ lib/spree_google_analytics.rb
@@ -2,3 +2,14 @@ require 'spree_core'
 require 'spree_extension'
 require 'spree_google_analytics/engine'
 require 'spree_google_analytics/version'
+
+module SpreeGoogleAnalytics
+  class << self
+    def webpacker
+      @webpacker ||= ::Webpacker::Instance.new(
+          root_path: SpreeGoogleAnalytics::Engine.root,
+          config_path: SpreeGoogleAnalytics::Engine.root.join('config', 'webpacker.yml')
+      )
+    end
+  end
+end
