d4849c16e566a7cb5fd2788126448900ee9cabb0
Grzegorz Tłuszcz <grzegorz.tluszcz@upsidelab.io>
2020-11-16 14:40:18 +0100

    Rename modules

---
+++ app/controllers/spree/admin/analytics_controller.rb
@@ -0,0 +1,7 @@
+module Spree
+  module Admin
+    class AnalyticsController < Spree::Admin::BaseController
+      def index; end
+    end
+  end
+end
--- app/controllers/spree_google_analytics/admin/analytics_controller.rb
+++
@@ -1,9 +0,0 @@
-require_dependency "spree_google_analytics/application_controller"
-
-module SpreeGoogleAnalytics
-  module Admin
-    class AnalyticsController < ApplicationController
-      def index; end
-    end
-  end
-end
