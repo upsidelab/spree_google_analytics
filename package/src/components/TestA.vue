ce61ef06025fed14329d968d400f0bb185ee71fb
Grzegorz Tłuszcz <grzegorz.tluszcz@upsidelab.io>
2020-11-18 14:52:38 +0100

    Add rollup

---
+++ package/src/components/TestA.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="example">{{ msg }}</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Hello from test A'
+    }
+  }
+}
+</script>
+
+<style>
+.example {
+  color: red;
+}
+</style>
---
+++ package/src/components/index.js
@@ -0,0 +1,7 @@
+import TestA from './TestA.vue'
+import TestB from './TestB.vue'
+
+export default {
+    TestA,
+    TestB
+}
