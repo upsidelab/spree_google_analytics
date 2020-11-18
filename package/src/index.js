03c75e4e32b4ee5cfdbaf358237f1ec2468a6ac0
Grzegorz Tłuszcz <grzegorz.tluszcz@upsidelab.io>
2020-11-18 16:04:49 +0100

    Test mounting

--- package/src/index.js
+++ package/src/index.js
@@ -2,31 +2,41 @@ import * as components from './components'
 import * as pages from './pages'
 
 // Declare install function executed by Vue.use()
-export function install(Vue) {
-    if (install.installed) return;
-    install.installed = true;
 
-    Object.keys(pages).forEach(name => {
-        const component = pages[name]
-        Vue.component(name, component);
-    })
-}
+document.addEventListener('DOMContentLoaded', () => {
+  const app = new Vue({
+    el: '#app',
+    components: pages
+  })
+})
 
-// Create module definition for Vue.use()
-const plugin = {
-    install,
-};
 
-// Auto-install when vue is found (eg. in browser via <script> tag)
-let GlobalVue = null;
-if (typeof window !== 'undefined') {
-    GlobalVue = window.Vue;
-} else if (typeof global !== 'undefined') {
-    GlobalVue = global.Vue;
-}
-if (GlobalVue) {
-    GlobalVue.use(plugin);
-}
+
+// export function install(Vue) {
+//     if (install.installed) return;
+//     install.installed = true;
+//
+//     Object.keys(pages).forEach(name => {
+//         const component = pages[name]
+//         Vue.component(name, component);
+//     })
+// }
+//
+// // Create module definition for Vue.use()
+// const plugin = {
+//     install,
+// };
+//
+// // Auto-install when vue is found (eg. in browser via <script> tag)
+// let GlobalVue = null;
+// if (typeof window !== 'undefined') {
+//     GlobalVue = window.Vue;
+// } else if (typeof global !== 'undefined') {
+//     GlobalVue = global.Vue;
+// }
+// if (GlobalVue) {
+//     GlobalVue.use(plugin);
+// }
 
 // To allow use as module (npm/webpack/etc.) export component
 export default {
