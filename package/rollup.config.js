8794b648bb167525d9d386b1f2c74fe61c67dd2e
Grzegorz Tłuszcz <grzegorz.tluszcz@upsidelab.io>
2020-11-19 13:17:48 +0100

    move package.json inside package

--- package/rollup.config.js
+++ package/rollup.config.js
@@ -2,7 +2,7 @@ import commonjs from '@rollup/plugin-commonjs'; // Convert CommonJS modules to E
 import vue from 'rollup-plugin-vue'; // Handle .vue SFC files
 import buble from '@rollup/plugin-buble'; // Transpile/polyfill with reasonable browser support
 export default {
-    input: './package/src/index.js', // Path relative to package.json
+    input: './src/index.js', // Path relative to package.json
     output: {
         name: 'index',
         exports: 'named',
