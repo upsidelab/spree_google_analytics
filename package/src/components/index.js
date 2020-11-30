936eec74b1b8b91a8dddcbedc5648500fd388994
Grzegorz Tłuszcz <grzegorz.tluszcz@upsidelab.io>
2020-11-30 17:47:55 +0100

    Add mixin to exported objects

--- package/src/components/index.js
+++ package/src/components/index.js
@@ -5,3 +5,5 @@ export { default as Spinner} from './utils/Spinner.vue'
 
 export { default as ReportsWithDate} from './reports/ReportsWithDate.vue'
 export { default as TotalsReport} from './reports/TotalsReport.vue'
+
+export { default as AnalyticsReportMixin} from './mixins/AnalyticsReportMixin'
