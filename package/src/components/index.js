68e9e31089b78039e77beb28243e5006df3aad45
Karol Szuster <karol.szuster@upsidelab.io>
2020-12-01 12:02:25 +0100

    Add PagesReport

--- package/src/components/index.js
+++ package/src/components/index.js
@@ -8,5 +8,6 @@ export { default as TotalsReport } from './reports/TotalsReport.vue'
 export { default as ProductsReport } from './reports/ProductsReport.vue'
 export { default as CategoriesReport } from './reports/CategoriesReport.vue'
 export { default as UsersReport } from './reports/UsersReport.vue'
+export { default as PagesReport } from './reports/PagesReport.vue'
 
 export { default as AnalyticsReportMixin } from './mixins/AnalyticsReportMixin'
---
+++ package/src/components/reports/PagesReport.vue
@@ -0,0 +1,34 @@
+<script>
+import AnalyticsReportMixin from "../mixins/AnalyticsReportMixin.js";
+
+export default {
+  mixins: [ AnalyticsReportMixin ],
+  data() {
+    return {
+      reportId: 'pages-report',
+      report: null
+    }
+  },
+  computed: {
+    query() {
+      return {
+        'ids': `ga:${this.gaViewId}`,
+        'start-date': this.startDate.toISOString().split('T')[0],
+        'end-date': this.endDate.toISOString().split('T')[0],
+        'metrics': 'ga:avgTimeOnPage,ga:pageViews',
+        'dimensions': 'ga:pagePath',
+        'filters': 'ga:pagePath=@product'
+      }
+    },
+    chart() {
+      return {
+        'container': this.reportId,
+        'type': 'TABLE',
+        'options': {
+          'width': '100%'
+        }
+      }
+    }
+  }
+}
+</script>
