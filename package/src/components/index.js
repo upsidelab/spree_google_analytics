e385c9317b1dc7501286b3ecacf84968004497f6
Grzegorz Tłuszcz <grzegorz.tluszcz@upsidelab.io>
2020-12-01 12:07:08 +0100

    Add Sales report

--- package/src/components/index.js
+++ package/src/components/index.js
@@ -8,5 +8,6 @@ export { default as TotalsReport } from './reports/TotalsReport.vue'
 export { default as ProductsReport } from './reports/ProductsReport.vue'
 export { default as CategoriesReport } from './reports/CategoriesReport.vue'
 export { default as UsersReport } from './reports/UsersReport.vue'
+export { default as SalesReport } from './reports/SalesReport.vue'
 
 export { default as AnalyticsReportMixin } from './mixins/AnalyticsReportMixin'
---
+++ package/src/components/reports/SalesReport.vue
@@ -0,0 +1,32 @@
+<script>
+import AnalyticsReportMixin from "../mixins/AnalyticsReportMixin.js";
+
+export default {
+  mixins: [ AnalyticsReportMixin ],
+  data() {
+    return {
+      reportId: 'sales-report',
+      report: null
+    }
+  },
+  computed: {
+    query() {
+      return {
+        'ids': `ga:${this.gaViewId}`,
+        'start-date': this.startDate.toISOString().split('T')[0],
+        'end-date': this.endDate.toISOString().split('T')[0],
+        'metrics': 'ga:transactions,ga:transactionRevenue,ga:revenuePerTransaction'
+      }
+    },
+    chart() {
+      return {
+        'container': this.reportId,
+        'type': 'TABLE',
+        'options': {
+          'width': '100%'
+        }
+      }
+    }
+  }
+}
+</script>
