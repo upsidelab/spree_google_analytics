330e4d67dac1cef1387062037deb1ec82c61e738
gtluszcz <gtluszcz@gmail.com>
2020-12-02 18:33:36 +0100

    Add users countries report

--- package/src/components/index.js
+++ package/src/components/index.js
@@ -13,6 +13,7 @@ export { default as SalesBreakdownReport } from './reports/SalesBreakdownReport.
 export { default as ProductPagesReport } from './reports/ProductPagesReport.vue'
 export { default as OtherPagesReport } from './reports/OtherPagesReport.vue'
 export { default as UsersSummaryReport } from './reports/UsersSummaryReport.vue'
+export { default as UsersCountriesReport } from './reports/UsersCountriesReport.vue'
 export { default as UsersChartReport } from './reports/UsersChartReport.vue'
 
 export { default as AnalyticsReportMixin } from './mixins/AnalyticsReportMixin'
