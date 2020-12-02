de67b6bb6cbd4f37b6de6ec359da7add7cdb3113
gtluszcz <gtluszcz@gmail.com>
2020-12-02 16:01:28 +0100

    Don't run migrations on install

--- lib/generators/spree_google_analytics/install/install_generator.rb
+++ lib/generators/spree_google_analytics/install/install_generator.rb
@@ -1,29 +1,12 @@
 module SpreeGoogleAnalytics
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      class_option :migrations, type: :boolean, default: true
-      class_option :auto_run_migrations, type: :boolean, default: false
       class_option :skip_webpacker_installer,
                    type: :boolean,
                    default: false,
                    desc: "Skip running the webpacker installer."
 
       source_root File.expand_path("files", __dir__)
-
-      def add_migrations
-        return unless options[:migrations]
-        run 'bundle exec rake railties:install:migrations FROM=spree_google_analytics'
-      end
-
-      def run_migrations
-        return unless options[:migrations]
-        run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask('Would you like to run the migrations now? [Y/n]'))
-        if run_migrations
-          run 'bundle exec rails db:migrate'
-        else
-          puts 'Skipping rails db:migrate, don\'t forget to run it!'
-        end
-      end
 
       def run_webpacker_installer
         return if options[:skip_webpacker_installer]
@@ -45,7 +28,7 @@ module SpreeGoogleAnalytics
         run "yarn add spree_google_analytics"
       end
 
-      def copy_alchemy_entry_point
+      def copy_js_entry_point
         webpack_config = YAML.load_file(app_root.join("config", "webpacker.yml"))[Rails.env]
         copy_file "spree_google_analytics.js",
                   app_root.join(webpack_config["source_path"], webpack_config["source_entry_path"], "spree_google_analytics.js")
